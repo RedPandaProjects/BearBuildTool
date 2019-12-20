c0c04893ad45b3df6d785501ae943484703ac0d8
Иван <i-sobolevskiy@mail.ru>
2019-12-20 17:13:40 +0700

    update auto build

--- pack_build.cmd
+++ pack_build.cmd
@@ -2,9 +2,5 @@ if %CONFIGURATION%==Release if "%PLATFORM%"=="Any CPU" goto :RUN
 goto :EXIT
 :RUN
 md temp
-cd bin\Debug
-copy *.* ..\..\temp
-cd ..\..\temp
-copy ..\Licence.txt 
-7z a Release.7z .\*
+7z a temp\release.7z bin\release
 :EXIT
